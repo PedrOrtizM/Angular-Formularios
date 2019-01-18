9fe73982a9e33d6320bf66ffeee49472e56468f4
PedrOrtizM <pedrortizm18_@hotmail.com>
2019-01-18 17:14:04 -0500

    aproximacion por data

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -3,35 +3,34 @@ import { FormGroup,FormControl,Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-data',
-  templateUrl: './data.component.html',
-  styleUrls: ['./data.component.css']
+  templateUrl: './data.component.html'
 })
 export class DataComponent {
 
   forma:FormGroup;
 
-  usuario:any={
+  usuario:Object={
     nombreCompleto:{
       nombre: "Pedro",
       apellido: "Ortiz"
     },
     correo: "pedrortizm@hotmail.com"
-
   }
+
   constructor() {
     this.forma = new FormGroup({
 
-       'nombreCompleto': new FormGroup({
-              'nombre': new FormControl('',[Validators.required,
-                                       Validators.minLength(3)]),
+     'nombreCompleto':    new FormGroup({
 
-              'apellido': new FormControl('',
-                                       [Validators.required,
-                                       Validators.minLength(3)])
+              'nombre':   new FormControl('',[ Validators.required,
+                                               Validators.minLength(3)]),
+
+              'apellido': new FormControl('',[ Validators.required,
+                                               Validators.minLength(3)])
        }),
       'correo': new FormControl('',
                                     [Validators.required,
-                                    Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
+                                     Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
 
               })
 
